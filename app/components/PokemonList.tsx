8c863d2f278b4dcbfb71d8ebfe4fc10a5202a748
Ari Rebouças <arireboucasdev@gmail.com>
2024-11-22 14:26:13 -0300

    fix: text correction

--- app/components/PokemonList.tsx
+++ app/components/PokemonList.tsx
@@ -98,9 +98,7 @@ const App: React.FC = () => {
         >
           Anterior
         </button>
-        <span className="px-4 py-2 bg-gray-100">
-          Página {page} de {Math.ceil(totalPokemons / 21)}
-        </span>
+        <span className="px-4 py-2 bg-gray-100">Página {page}</span>
         <button
           onClick={() => handlePageChange(page + 1)}
           disabled={page * 21 >= totalPokemons}
